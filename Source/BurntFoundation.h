2eed7f6d51ff54ef63d64e2c1adfb2f7c8858437
Patrick Smith <pgwsmith@gmail.com>
2015-05-16 19:48:57 +1000

    Added iOS Framework, moved files

--- Source/BurntFoundation.h
+++ Source/BurntFoundation.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Burnt Caramel. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for BurntFoundation.
 FOUNDATION_EXPORT double BurntFoundationVersionNumber;
